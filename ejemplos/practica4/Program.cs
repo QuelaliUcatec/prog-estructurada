ebcf26be280076ecca20eb0021e873f6a80ee570
Sergio Navarro <sergio.denn.navarro@gmail.com>
2026-02-18 21:58:19 -0400

    agregando pratica 4

---
+++ ejemplos/practica4/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el primer numero: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo numero: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("La suma es: " + (num1 + num2));
+    }
+}
